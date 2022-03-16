5f7c689915bc1288ee7769a140e42cc1a5744e88
jrenfroe <joan.renfroe@gmail.com>
2022-03-15 19:30:53 -0700

    adds game board and squares

--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -1,1 +1,52 @@
 console.log("JS is running");
+
+
+/*----- constants -----*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+
+
+
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+
+
+
+
+/*----- functions -----*/
