7d55b721db9f3668ee495827ff6005a54e24dab2
jrenfroe <joan.renfroe@gmail.com>
2022-03-15 20:29:09 -0700

    adds game board styling

--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -2,33 +2,31 @@ console.log("JS is running");
 
 
 /*----- constants -----*/
-
-
-
-
-
-
-
-
-
+// X & O - Players
+// winning combinations
 
 
 
 /*----- app's state (variables) -----*/
 
+//where users click (where to put an x or o) - make sure a square is not taken first
+//Who's turn is it?
+//when we have a winner or if there is a tie - if game is ongoing
 
 
 
-
-
-
-
+//These are stretch goals for your game
+//Bonus - if player wants to resign (make a button)
+//Bonus - score for games won
+//Bonus - timer for game turn
 
 
 
 /*----- cached element references -----*/
 
-
+//things we will refer to often
+//the game squares
+//reset button
 
 
 
